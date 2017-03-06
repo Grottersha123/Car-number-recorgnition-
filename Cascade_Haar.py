bec02eb0643d2828c254cfe558fbb9744634c2fc
Grottersha123 <grottersha123@gmail.com>
2017-03-06 12:49:08 +0300

    Детектирование номера по каскаду хаара

---
+++ Cascade_Haar.py
@@ -0,0 +1,48 @@
+from urllib.request import urlopen
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+url = 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_russian_plate_number.xml'
+
+
+#Cкачиваение тренировочного файла
+def download(url):
+    logo = urlopen(url).read()
+    f = open("haarcascade_russian_plate_number.xml", "wb")
+    f.write(logo)
+    f.close()
+# Показ одной картинки
+def open_picture(img):
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#Показ двух картинок
+def open_two(img1,img2):
+    plt.subplot(121)
+    plt.imshow(img1,cmap='gray')
+    plt.title('Original Image')
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(122)
+    plt.imshow(img2,cmap='gray')
+    plt.title('Plate Image'), plt.xticks([]), plt.yticks([])
+    plt.show()
+
+path = './cars/1.png'
+plate_cascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
+img = cv2.imread(path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+plate = plate_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in plate:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,80,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+
+
+open_picture(roi_color)
+#open_two(img,roi_color)
+
+
+
