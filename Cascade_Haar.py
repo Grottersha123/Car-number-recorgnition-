70e65e85bcad5d9cba534aed5768c411d795cc55
Grottersha123 <grottersha123@gmail.com>
2017-03-08 04:27:28 +0300

    Добавлено задание вкр, добавлены пару новых функций

--- Cascade_Haar.py
+++ Cascade_Haar.py
@@ -12,7 +12,9 @@ def download(url):
     f.write(logo)
     f.close()
 # Показ одной картинки
+
 def open_picture(img):
+    img = cv2.imread(img)
     cv2.imshow('image',img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -30,18 +32,20 @@ def open_two(img1,img2):
     plt.show()
 
 path = './cars/1.png'
-plate_cascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
-img = cv2.imread(path)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+path1 = 'gray.png'
+def c(path):
+    plate_cascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-plate = plate_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in plate:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,80,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-
-
-open_picture(roi_color)
+    plate = plate_cascade.detectMultiScale(gray, 1.3,5)
+    for (x, y, w, h) in plate:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 80, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+    cv2.imwrite('gray.png',roi_color)
+c(path)
+open_picture(path1)
 #open_two(img,roi_color)
 
 
